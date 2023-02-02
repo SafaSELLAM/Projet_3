0bcb41d4f5912eeb62d2f3887fa1d67c9c9d4696
SafaSELLAM <safa.sellam@outlook.fr>
2023-02-02 09:57:57 +0100

    fix: fixed function login

--- FrontEnd/logIn.js
+++ FrontEnd/logIn.js
@@ -1,52 +1,33 @@
-const form = document.querySelector("#LogIn_form");
- const email = document.querySelector("#email_login").value;
- const password = document.querySelector("#password_login").value;
+const userEmail = "sophie.bluel@test.tld"; 
+const userPassword = "S0phie";
+
+ const form = document.querySelector("form");
+ const emailInput = document.querySelector("input[name='email']");
+ const passwordInput = document.querySelector("input[name='password']");
  const errorMsg =  document.querySelector("#error_msg");
- const userLogin = {
-    email:"sophie.bluel@test.tld",
-    password : "S0phie",
-    token : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4",
- };
- const chargeUtile = JSON.stringify(email , password);
 
- form.addEventListener("submit",function(event){
+
+ const chargeUtile = JSON.stringify(emailInput , passwordInput);
+
+ form.addEventListener("submit", function (event) {
     event.preventDefault();
-    if (email === "" || password ===""){
-        email="";
-        password="";
-        errorMsg.textContent="tous les champs doivent être remplis";
-        setTimeout(() => {
-            errorMsg.textContent="";
-        }, 2000);
-    } else if (
-        email !== userLogin.email ||
-        password!== userLogin.password
-    ){
-        email="";
-        password="";
-        errorMsg.textContent="Erreur dans l'identifiant ou le mot de passe";
-        setTimeout(() => {
-            errorMsg.textContent="";
-        }, 2000);
-    }else {
-        Login();
+  
+    // Get the values from the input fields
+    const enteredEmail = emailInput.value;
+    const enteredPassword = passwordInput.value;
+  
+    // Validate the input
+    if (enteredEmail === userEmail && enteredPassword === userPassword) {
+      // Generate the token
+    const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4";
+  
+      // Store the token in local storage
+      localStorage.setItem("token", token);
+  
+      // Redirect to the protected page
+      window.location.href = "index.html";
+    } else {
+      // Show an error message if the input is invalid
+      errorMsg.textContent="Erreur dans l'identifiant ou le mot de passe";
     }
- });
-
-
-
- function Login(){
-    fetch("http://localhost:5678/api/users/login",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json",
-            "Authorization": "Bearer " + token,
-        },
-        body:chargeUtile,
-    }).then(response => response.json()).then(data => {
-        if(data.token){
-            localStorage.setItem("token",data.token);
-            window.location.replace('index.html');
-        }
-    })
- }
+  });
