3d0a36653117199139e467eebece810abf36215b
SafaSELLAM <safa.sellam@outlook.fr>
2023-02-04 17:19:12 +0100

    fix: optimized function login

--- FrontEnd/logIn.js
+++ FrontEnd/logIn.js
@@ -5,28 +5,9 @@ const userPassword = "S0phie";
  const emailInput = document.querySelector("input[name='email']");
  const passwordInput = document.querySelector("input[name='password']");
  const errorMsg =  document.querySelector("#error_msg");
- const userIsLogin = true; 
-
- const Hidden1 = document.querySelector(".modale_header");
- const Hidden2 = document.querySelector(".fa-pen-to-square");
- const Hidden3 = document.querySelector(".text_modale");
- const Hidden4 = document.querySelector(".modale_bouton_header");
-
-
- localStorage.getItem("userIsLogin",true);
- if(userIsLogin){
-
-   Hidden1.classList.remove("hidden");
-   Hidden2.classList.remove("hidden");
-   Hidden3.classList.remove("hidden");
-   Hidden4.classList.remove("hidden");
- }
- 
-
 
  form.addEventListener("submit", function (event) {
     event.preventDefault();
-  
     // Get the values from the input fields
     const enteredEmail = emailInput.value;
     const enteredPassword = passwordInput.value;
@@ -44,5 +25,7 @@ const userPassword = "S0phie";
     } else {
       // Show an error message if the input is invalid
       errorMsg.textContent="Erreur dans l'identifiant ou le mot de passe";
+      localStorage.setItem("token","");
+      localStorage.setItem("userIsLogin",false);
     }
   });
