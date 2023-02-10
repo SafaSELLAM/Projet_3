a86137515867ad83c47614bfbef88d06f642c2bc
SafaSELLAM <safa.sellam@outlook.fr>
2023-02-10 09:20:53 +0100

    docs: add modal 1st page

--- FrontEnd/administrator.js
+++ FrontEnd/administrator.js
@@ -9,7 +9,7 @@ if(userIsLogin){
     const token = localStorage.getItem("token");
     logout_btn.style.display = "block";
     login_btn.style.display = "none";
-    header_info.style.display="none";
+    header_info.style.display= "none";
     listUpdateElements.forEach(element => {
         console.log(element)
         const updateEL = document.getElementById(element);
@@ -32,3 +32,34 @@ logout_btn.addEventListener("click", function(){
     localStorage.setItem("userIsLogin",false)
     localStorage.setItem("token","")
 })
+//ajout boîte modale
+let modal = null;
+
+const openModal = function(e){
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute('href'))
+    target.style.display=null;
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal','true')
+    modal=target;
+    modal.addEventListener('click',closeModal)
+    modal.querySelector('.js_modal_close').addEventListener('click',closeModal)
+    modal.querySelector('.js_modal_stop').addEventListener('click',stopPropagation)
+}
+const closeModal= function(e){
+    e.preventDefault()
+   // if (modal === null) return
+    modal.style.display="none";
+    modal.setAttribute('aria-hidden','true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click',closeModal)
+    modal.querySelector('.js_modal_close').removeEventListener('click',closeModal)
+    modal.querySelector('.js_modal_stop').removeEventListener('click',stopPropagation)
+    modal=null
+}
+const stopPropagation = function (e){
+    e.stopPropagation()
+}
+document.querySelectorAll('.js_modal').forEach(a=>{
+    a.addEventListener('click',openModal)
+});
