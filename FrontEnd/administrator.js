77aa752d9bb1279394b141a0a9cd2eff645e275d
SafaSELLAM <safa.sellam@outlook.fr>
2023-02-14 10:36:04 +0100

    docs : add open and close modal

--- FrontEnd/administrator.js
+++ FrontEnd/administrator.js
@@ -1,3 +1,5 @@
+
+//Elements page administrateur
 const userIsLogin = JSON.parse(localStorage.getItem("userIsLogin"));
 console.log(userIsLogin)
 const logout_btn = document.getElementById("logoutBtn");
@@ -6,7 +8,6 @@ const listUpdateElements = ['updateHeader', 'updateImg', 'updateInfo', 'updatePr
 const header_info = document.getElementById("header_info");
 
 if(userIsLogin){
-    const token = localStorage.getItem("token");
     logout_btn.style.display = "block";
     login_btn.style.display = "none";
     header_info.style.display= "none";
@@ -32,23 +33,29 @@ logout_btn.addEventListener("click", function(){
     localStorage.setItem("userIsLogin",false)
     localStorage.setItem("token","")
 })
+
+
+
+
 //ajout boîte modale
 let modal = null;
-
-const openModal = function(e){
+//const galleryModal = document.getElementsByClassName("gallery_modal")[0];
+const openModal = function(e) {
+  e.preventDefault();
+  const target = document.querySelector(e.target.getAttribute("href"));
+  target.style.display = null;
+  target.removeAttribute("aria-hidden");
+  target.setAttribute("aria-modal", "true");
+ 
+  modal = target;
+  modal.addEventListener("click", closeModal);
+  modal.querySelector(".js_modal_close").addEventListener("click", closeModal);
+  modal.querySelector(".js_modal_stop").addEventListener("click", stopPropagation);
+};
+  //fermer la modale 
+  const closeModal= function(e){
     e.preventDefault()
-    const target = document.querySelector(e.target.getAttribute('href'))
-    target.style.display=null;
-    target.removeAttribute('aria-hidden');
-    target.setAttribute('aria-modal','true')
-    modal=target;
-    modal.addEventListener('click',closeModal)
-    modal.querySelector('.js_modal_close').addEventListener('click',closeModal)
-    modal.querySelector('.js_modal_stop').addEventListener('click',stopPropagation)
-}
-const closeModal= function(e){
-    e.preventDefault()
-   // if (modal === null) return
+  
     modal.style.display="none";
     modal.setAttribute('aria-hidden','true');
     modal.removeAttribute('aria-modal');
@@ -56,10 +63,48 @@ const closeModal= function(e){
     modal.querySelector('.js_modal_close').removeEventListener('click',closeModal)
     modal.querySelector('.js_modal_stop').removeEventListener('click',stopPropagation)
     modal=null
+    
 }
 const stopPropagation = function (e){
     e.stopPropagation()
 }
+document.querySelectorAll('.js_modal_stop').forEach(a=>{
+    a.addEventListener('click', stopPropagation)
+});
+document.querySelectorAll('.js_modal_close').forEach(a=>{
+    a.addEventListener('click', closeModal)
+});
 document.querySelectorAll('.js_modal').forEach(a=>{
     a.addEventListener('click',openModal)
 });
+  // Ouvrir la deuxième modale
+  
+    document.getElementById("openModal2").addEventListener("click", function() {
+    document.querySelector(".modal_wrapper").style.display="none";
+    document.getElementById("modal2").style.display=null;
+  });
+//retour première modale
+  const backToModal1 = document.querySelector(".js_back_modal");
+    backToModal1.addEventListener("click", function () {
+    document.getElementById("modal2").style.display="none";
+    document.querySelector(".modal_wrapper").style.display=null;
+  });
+
+//selecteur categories
+const reponse3 = await fetch ('http://localhost:5678/api/categories');
+const categories = await reponse3.json()
+categories.unshift({ name: "Sélectionnez une option", id: "" });
+
+    let select = document.querySelector("#categories");
+
+    categories.forEach(element=>{
+
+        let  optionElement = document.createElement("option");
+        optionElement.innerText= element.name;
+        optionElement.dataset.id= element.id;
+        select.appendChild(optionElement)
+    });
+
+    
+ 
+
